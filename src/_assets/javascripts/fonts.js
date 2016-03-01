719b458e6bfc5250107f6713da2cbf3eb9fd8d69
Evan Sosenko <razorx@evansosenko.com>
2016-03-01 00:14:18 -0800

    Add Monoid font

--- src/_assets/javascripts/fonts.js
+++ src/_assets/javascripts/fonts.js
@@ -2,4 +2,13 @@
 
 'use strict'
 
-WebFont.load()
+WebFont.load({
+  custom: {
+    families: [
+      'Monoid Regular', 'Monoid Bold', 'Monoid Italic'
+    ],
+    urls: [
+      '{% asset_path vendor/monoid %}'
+    ]
+  }
+})
