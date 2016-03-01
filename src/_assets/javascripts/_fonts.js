b69242941fd19b144d53aaa8137bce1863fb784f
Evan Sosenko <razorx@evansosenko.com>
2016-03-01 13:53:48 -0800

    Load KaTeX fonts with Web Font Loader

--- src/_assets/javascripts/_fonts.js
+++ src/_assets/javascripts/_fonts.js
@@ -5,10 +5,16 @@
 WebFont.load({
   custom: {
     families: [
-      'Monoid Regular', 'Monoid Bold', 'Monoid Italic'
+      'Monoid Regular', 'Monoid Bold', 'Monoid Italic',
+      'KaTeX_AMS', 'KaTeX_Caligraphic', 'KaTeX_Caligraphic', 'KaTeX_Fraktur',
+      'KaTeX_Fraktur', 'KaTeX_Main', 'KaTeX_Main', 'KaTeX_Main', 'KaTeX_Math',
+      'KaTeX_Math', 'KaTeX_Math', 'KaTeX_SansSerif', 'KaTeX_SansSerif',
+      'KaTeX_SansSerif', 'KaTeX_Script', 'KaTeX_Size1', 'KaTeX_Size2',
+      'KaTeX_Size3', 'KaTeX_Size4', 'KaTeX_Typewriter'
     ],
     urls: [
-      '{% asset_path vendor/monoid %}'
+      '{% asset_path vendor/monoid %}',
+      '{{ site.data.vendor.katex.src }}'
     ]
   }
 })
