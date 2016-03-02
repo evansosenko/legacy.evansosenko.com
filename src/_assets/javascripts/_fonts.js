19012a9f25eef1792575ad3c96c079982363c5ec
Evan Sosenko <razorx@evansosenko.com>
2016-03-01 20:03:54 -0800

    Move font families to vendor data

--- src/_assets/javascripts/_fonts.js
+++ src/_assets/javascripts/_fonts.js
@@ -5,12 +5,8 @@
 WebFont.load({
   custom: {
     families: [
-      'Monoid Regular', 'Monoid Bold', 'Monoid Italic',
-      'KaTeX_AMS', 'KaTeX_Caligraphic', 'KaTeX_Caligraphic', 'KaTeX_Fraktur',
-      'KaTeX_Fraktur', 'KaTeX_Main', 'KaTeX_Main', 'KaTeX_Main', 'KaTeX_Math',
-      'KaTeX_Math', 'KaTeX_Math', 'KaTeX_SansSerif', 'KaTeX_SansSerif',
-      'KaTeX_SansSerif', 'KaTeX_Script', 'KaTeX_Size1', 'KaTeX_Size2',
-      'KaTeX_Size3', 'KaTeX_Size4', 'KaTeX_Typewriter'
+      '{{ site.data.vendor.monoid.families | join:"', '" }}',
+      '{{ site.data.vendor.katex.families | join:"', '" }}'
     ],
     urls: [
       '{% asset_path vendor/monoid %}',
