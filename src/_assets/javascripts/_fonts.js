d5e65a70eb9f4a927cd4638e69d7feae5b2843f1
Evan Sosenko <razorx@evansosenko.com>
2016-03-01 20:39:14 -0800

    Add Material Design Iconic Font

--- src/_assets/javascripts/_fonts.js
+++ src/_assets/javascripts/_fonts.js
@@ -6,10 +6,12 @@ WebFont.load({
   custom: {
     families: [
       '{{ site.data.vendor.monoid.families | join:"', '" }}',
+      '{{ site.data.vendor.zmdi.families | join:"', '" }}',
       '{{ site.data.vendor.katex.families | join:"', '" }}'
     ],
     urls: [
       '{% asset_path vendor/monoid %}',
+      '{{ site.data.vendor.zmdi.src }}',
       '{{ site.data.vendor.katex.src }}'
     ]
   }
