14e50e5f2ff9816d4b0031c734bdf19a9b94a923
Evan Sosenko <razorx@evansosenko.com>
2016-04-21 20:58:39 -0700

    Add Overpass font

--- src/_assets/javascripts/_fonts.js
+++ src/_assets/javascripts/_fonts.js
@@ -12,11 +12,13 @@ WebFont.load({
   },
   custom: {
     families: [
+      '{{ site.data.vendor.overpass.families | join:"', '" }}',
       '{{ site.data.vendor.monoid.families | join:"', '" }}',
       '{{ site.data.vendor.zmdi.families | join:"', '" }}',
       '{{ site.data.vendor.katex.families | join:"', '" }}'
     ],
     urls: [
+      '{{ site.data.vendor.overpass.src }}',
       '{% asset_path vendor/monoid %}',
       '{{ site.data.vendor.zmdi.src }}',
       '{{ site.data.vendor.katex.src }}'
