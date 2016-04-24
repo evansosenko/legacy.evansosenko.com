cb5a643e912569c79c2bcd8e0e54705e722945fd
Evan Sosenko <razorx@evansosenko.com>
2016-04-23 18:49:41 -0700

    Load katex fonts async and only on math pages

--- src/_assets/javascripts/_fonts.js
+++ src/_assets/javascripts/_fonts.js
@@ -12,14 +12,12 @@ window.WebFontConfig = {
     families: [
       '{{ site.data.vendor.overpass.families | join:"', '" }}',
       '{{ site.data.vendor.monoid.families | join:"', '" }}',
-      '{{ site.data.vendor.zmdi.families | join:"', '" }}',
-      '{{ site.data.vendor.katex.families | join:"', '" }}'
+      '{{ site.data.vendor.zmdi.families | join:"', '" }}'
     ],
     urls: [
       '{{ site.data.vendor.overpass.src }}',
       '{% asset_path vendor/monoid %}',
-      '{{ site.data.vendor.zmdi.src }}',
-      '{{ site.data.vendor.katex.src }}'
+      '{{ site.data.vendor.zmdi.src }}'
     ]
   }
 }
--- src/_assets/javascripts/vendor/katex.js
+++ src/_assets/javascripts/vendor/katex.js
@@ -1,8 +1,21 @@
 //= require katex
 //= require katex/dist/contrib/auto-render.min
 
-/* global renderMathInElement */
+/* global renderMathInElement, WebFont */
 
 'use strict'
 
-renderMathInElement(document.body)
+document.addEventListener('DOMContentLoaded', function () {
+  WebFont.load({
+    custom: {
+      families: [
+        '{{ site.data.vendor.katex.families | join:"', '" }}'
+      ],
+      urls: [
+        '{{ site.data.vendor.katex.src }}'
+      ]
+    }
+  })
+
+  renderMathInElement(document.body)
+})
