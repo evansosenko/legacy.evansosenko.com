f6784cc3ce67b81a273a9b7bd83fb6b961dd98c6
Evan Sosenko <razorx@evansosenko.com>
2016-04-25 18:42:05 -0700

    Deploy 4102bcc from v1.0.0

--- sw-import.js
+++ sw-import.js
@@ -1,1 +1,1 @@
-importScripts("https://evansosenko.com/assets/platinum-sw/service-worker-c2d40749d6ee3ab44671022c1e752c83c2f52b8ea119dba814d03daaa2a71500.js");
+importScripts("https://evansosenko.netlify.com/assets/platinum-sw/service-worker-c2d40749d6ee3ab44671022c1e752c83c2f52b8ea119dba814d03daaa2a71500.js");
