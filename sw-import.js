f2de337a58fec56ad2706b0888686d2bd1b1e789
Evan Sosenko <razorx@evansosenko.com>
2016-05-09 11:51:37 -0700

    Deploy 6d76fcc from v1.0.0

--- sw-import.js
+++
@@ -1,8 +0,0 @@
-// Chain-loads the following files:
-// /assets/platinum-sw/bootstrap/offline-analytics-2aea19772056c17cd98c88828ae323474ced15e158e6d43b3b10d67d5e56ccda.js
-// /assets/platinum-sw/bootstrap/simple-db-f210fd2dd09b9fed21a29bcb5e0a6c04758b5b7e9130e5156fb2eaef6faf8e48.js
-// /assets/platinum-sw/bootstrap/sw-toolbox-setup-7a61ac89bec9d01bef4832b546e57ea89289b6787f712a89f39020b5cdf489c8.js
-// /assets/sw-toolbox/sw-toolbox-395835b36bbc5792f661bf1d461cf670f580f3ca6e552aea867881abe6dfa9f3.js
-// /assets/sw-toolbox/sw-toolbox.map-b95927e9c73b4fe4cfed475e4fca11e9514caa00e08454254744ed42803ade8d.json
-
-importScripts('/assets/platinum-sw/service-worker-c2d40749d6ee3ab44671022c1e752c83c2f52b8ea119dba814d03daaa2a71500.js')
