c8aec2287e0edc062eb238535f525a60d4650119
Evan Sosenko <razorx@evansosenko.com>
2016-05-09 17:39:55 -0700

    Load Prism async

--- src/_assets/javascripts/vendor/prism.js
+++ src/_assets/javascripts/vendor/prism.js
@@ -1,1 +1,15 @@
 //= require prism
+
+(function () {
+  'use strict'
+
+  function init () {
+    window.Prism.highlightAll()
+  }
+
+  if (document.readyState !== 'loading') {
+    init()
+  } else {
+    document.addEventListener('DOMContentLoaded', init)
+  }
+})()
