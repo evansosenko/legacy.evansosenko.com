cc28dfbe7991d63432f399ac794827247625018c
Evan Sosenko <razorx@evansosenko.com>
2016-05-09 20:04:00 -0700

    Load disqus async

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -1,5 +1,4 @@
 //= require console-polyfill
 //= require fastclick
 //= require headroom.js/dist/headroom
-//= require vendor/disqus
 //= require _init
--- src/_assets/javascripts/vendor/disqus.js
+++ src/_assets/javascripts/vendor/disqus.js
@@ -1,8 +1,23 @@
 'use strict'
 
-var disqus_config = function () {
-  {% if jekyll.environment == 'production' %}
-    this.page.url = '{{ site.url }}{{ site.basename }}{{ page.url }}'
-  {% endif %}
+window.disqus_config = function () {
+{% if jekyll.environment == 'production' %}
+  this.page.url = '{{ site.url }}{{ site.basename }}{{ page.url }}'
+{% endif %}
   this.page.identifier = '{{ page.url }}'
 }
+
+{% if jekyll.environment == 'production' %}
+  {% assign shortname = site.data.vendor.disqus.shortname %}
+{% else %}
+  {% assign shortname = site.data.vendor.disqus.devshortname %}
+{% endif %}
+
+;(function (d) {
+  var el = d.createElement('script')
+  var s = d.scripts[0]
+  el.async = 1
+  el.src = 'https://{{ shortname }}.{{ site.data.vendor.disqus.src }}'
+  s.parentNode.insertBefore(el, s)
+  s.setAttribute('data-timestamp', +new Date());
+})(document)
